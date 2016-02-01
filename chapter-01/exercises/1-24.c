023a568d4b00f11d96d325566c9bc12e44a3d422
Ricky Manning <rlcm@bts.co.uk>
2016-02-01 19:15:57 +0000

    Add Exercise 1-24.c placeholder
    This is a placeholder for Exercise 1-24. I need to get on with my C
    studies. Whilst potentially fun, is not important that I spend time
    doing this now. I will endeavour to return to this and complete it in
    the near future

---
+++ chapter-01/exercises/1-24.c
@@ -0,0 +1,16 @@
+/* 1-24 Write a program to check a C program for rudimentary syntax errors
+like unbalanced parentheses, brackets and braces. Don't forget about quotes,
+both single and double, escape sequences, and comments. (This program is
+hard if you do it in full generality), page 34 */
+
+#include <stdio.h>
+
+main()
+{
+    /* This is a placeholder for Exercise 1-24. I need to get on with my C
+    studies. Whilst potentially fun, is not important that I spend time
+    doing this now. I will endeavour to return to this and complete it in
+    the near future */
+    
+    return 0;
+}
